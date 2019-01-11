83b8ba25b5776419819d2c036e007684038892fe
cyproto <kyashkarande@gmail.com>
2019-01-11 12:34:12 +0000

    Viper pie bringup

--- viper_z2_plus.mk
+++ viper_z2_plus.mk
@@ -21,11 +21,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/zuk/z2_plus/device.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/carbon/config/common.mk)
-$(call inherit-product, vendor/carbon/config/gsm.mk)
+$(call inherit-product, vendor/viper/config/common_full_phone.mk)
+#$(call inherit-product, vendor/carbon/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := carbon_z2_plus
+PRODUCT_NAME := viper_z2_plus
 PRODUCT_DEVICE := z2_plus
 PRODUCT_BRAND := ZUK
 PRODUCT_MODEL := Z2 Plus
